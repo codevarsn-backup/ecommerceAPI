fb35d6c286b084d86c4a91197cb921d05bc7fb61
codevarsn-backup <135983184+codevarsn-backup@users.noreply.github.com>
2023-06-08 10:10:40 -0500

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,12 @@
-const { Router } = require("express");
+const express = require("express");
 const { createUserValidator, updateUserValidator } = require("../validators/user.validator");
 const { createUser, updateUser } = require("../controllers/user.controllers");
-const multerUpload = require("../middlewares/upload.middleware")
+const multerUpload = require("../middlewares/upload.middleware");
 
-const router = Router();
+const router = express.Router();
 
 router.post("/api/v1/users", createUserValidator, createUser);
 
 router.put("/api/v1/users/:id", multerUpload.single('file'), updateUser);
-
-
 
 module.exports = router;
