f97eef199145db6a508952579b3f0d31a1dc374f
codevarsn <mariano5623@gmail.com>
2023-06-08 01:28:13 -0500

    first commit

---
+++ src/utils/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+require('dotenv').config()
+
+const db = new Sequelize({
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dialect: "postgres",
+  logging: false,
+});
+
+module.exports = db;
